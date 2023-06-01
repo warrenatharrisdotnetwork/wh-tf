eb0151658d9597686af124b2ab53f6a61221bd54
warren@harris.network <warren@harris.network>
2023-06-01 09:21:41 -0400

    added yesterdays work

---
+++ main.tf
@@ -0,0 +1,57 @@
+resource "aws_vpc" "warren_vpc_1" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_support = true
+    enable_dns_hostnames = true
+    # below is the default, not required
+    instance_tenancy = "default"
+    tags = {
+      Name = "warren"
+      bird = "word"
+
+    }
+  
+}
+
+resource "aws_internet_gateway" "ig1" {
+  vpc_id = aws_vpc.main.id
+
+}
+
+resource "aws_subnet" "public1" {
+  availability_zone = "us-east-1a"
+  cidr_block = "10.0.0.0/24"
+}
+
+resource "aws_subnet" "public2" {
+  availability_zone = "us-east-1b"
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_subnet" "private1" {
+    availability_zone = "us-east-1a"
+    cidr_block = "10.0.2.0/24"
+}
+
+resource "aws_route_table" "public-rt1" {
+  vpc_id = aws_vpc.warren_vpc_1
+  tags = {
+    bird = "word"
+    Name = "warren-private-rt"
+  }
+  
+}
+
+resource "aws_route" "public-rt-1" {
+  route_table_id = aws_route_table.public-rt1
+  # only default route
+  destinationdestination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.ig1.id
+  
+}
+
+resource "aws_route_table_association" "rt-public" {
+  # associate multiple subnets with the route table above for public subnets
+  subnet_id = [aws_subnet.public1.id,aws_subnet.public2]
+  route_table_id = aws_route_table.public-rt1.id
+}
+
